4e96c79f7d72c4d6ddc1dd9deec51b941e1865f4
CodeMonkeyInit <kyliev09@gmail.com>
2019-02-08 18:43:28 +0300

    Example

---
+++ App/App.js
@@ -0,0 +1,12 @@
+
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <div className="App">
+                <NavBar/>
+            </div>
+        );
+    }
+}
---
+++ App/NavBar/NavBar.js
@@ -0,0 +1,42 @@
+class NavBar extends React.Component {
+
+    navbarElements = [
+        {key: "Дом", value: null},
+        {key: "Макросы", value: null}
+    ];
+
+    state = {
+        selectedKey: this.navbarElements[0].key,
+        navBarElements: this.navbarElements
+    };
+
+    render() {
+        return (
+            <div className="Nav-bar">
+                <div className="nav-bar-header">
+                    <span className="nav-bar-header-element">Умный дом</span>
+                    <div className="nav-bar-header-element nav-bar-expand">^</div>
+                </div>
+
+                <div className="nav-bar-body">
+                    {this.state.navBarElements.map((element, i) => (
+                        <div className={this.getElementClass(element)}
+                             onClick={() => this.itemSelected(element, i)}
+                             key={i}>
+                            {element.key}
+                        </div>
+                    ))}
+                </div>
+            </div>
+        );
+    }
+
+    getElementClass = element => (element.key === this.state.selectedKey ? 'nav-bar-item-selected' : "")
+        + " nav-bar-item";
+
+    itemSelected(element) {
+        this.setState({selectedKey: element.key});
+
+        return this.props.selected(element.value);
+    }
+}
